860f01c2ce74b0e05b182b5287e097f391c2d21e
medzerai <mohamed0zerai@gmail.com>
2022-01-26 21:28:06 +0100

    setting up the Landing page without styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Landing from "./pages/Landing";
 function App() {
   return (
     <div>
       <h1>TheJob</h1>
+      <Landing />
     </div>
   );
 }
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main_job.svg";
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="TheJob" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+
+          <p>
+            I'm baby asymmetrical put a bird on it dreamcatcher squid. Portland
+            shoreditch organic cloud bread vexillologist before they sold out
+            leggings. Tacos actually distillery, pickled letterpress mustache
+            unicorn taiyaki truffaut umami af. Lomo beard intelligentsia prism
+            gastropub offal freegan hashtag cray franzen deep v before they sold
+            out subway tile try-hard.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job search" className="img main-img" />
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
