14d6eeee4d14199e80829fde209129f6025a1196
medzerai <mohamed0zerai@gmail.com>
2022-01-27 14:03:27 +0100

    styling the Landing page with styled component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,9 @@
 import React from "react";
 import Landing from "./pages/Landing";
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: red;
-  color: white;
-  font-size: 1rem;
-`;
-const ButtonSecond = styled.button`
-  background: blue;
-  color: white;
-  font-size: 1rem;
-`;
 
 function App() {
   return (
     <div>
-      <Button>Click me</Button>
-      <ButtonSecond>Click me</ButtonSecond>
-      <h1>TheJob</h1>
       <Landing />
     </div>
   );
