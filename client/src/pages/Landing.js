b9c29cff0bdd589080e9f96f6e992f41b46bd13c
medzerai <mohamed0zerai@gmail.com>
2022-01-27 14:40:31 +0100

    Wrappers setup

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main_job.svg";
-import styled from "styled-components";
+import Wrapper from "../assets/wrappers/testing";
 
 const Landing = () => {
   return (
@@ -30,46 +30,5 @@ const Landing = () => {
     </Wrapper>
   );
 };
-
-const Wrapper = styled.main`
-  nav {
-    width: var(--fluid-width);
-    max-width: var(--max-width);
-    margin: 0 auto;
-    height: var(--nav-height);
-    display: flex;
-    align-items: center;
-    img {
-      height: 85%;
-    }
-  }
-  .page {
-    min-height: calc(100vh - var(--nav-height));
-    display: grid;
-    align-items: center;
-    margin-top: -3rem;
-  }
-  h1 {
-    font-weight: 700;
-    span {
-      color: var(--primary-500);
-    }
-  }
-  p {
-    color: var(--gray-600);
-  }
-  .main-img {
-    display: none;
-  }
-  @media (min-width: 992px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-    .main-img {
-      display: block;
-    }
-  }
-`;
 
 export default Landing;
