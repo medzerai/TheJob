1582a0a754b5c284a0206a25e8a2837b313a84a8
medzerai <mohamed0zerai@gmail.com>
2022-01-30 22:23:17 +0100

    upgrade the Alert

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,8 @@
 import React, { useReducer, useContext } from "react";
+
 import reducer from "./reducer";
+
+import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
 
 const initialState = {
   isLoading: false,
@@ -13,8 +16,21 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+    clearAlert();
+  };
+
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({ type: CLEAR_ALERT });
+    }, 3000);
+  };
+
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, displayAlert }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
